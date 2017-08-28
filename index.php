2349b72cd893e15944f67bd7c504ee684bf07f8d
Pitz Michaël <michaeljpitz@gmail.com>
2017-08-28 11:52:09 +0200

    vendor de bootstrap

--- index.php
+++ index.php
@@ -10,6 +10,8 @@ session_start();
 // dépendances
 require_once 'config.php';
 require_once 'm/myPDO.php';
+// dépendances composer
+require_once 'vendor/autoload.php';
 
 // connexion
 $connect = new myPDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=".DB_CHARSET.";port=".DB_PORT, DB_LOGIN, DB_PWD);
